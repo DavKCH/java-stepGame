2569dbb9ec7970225b585c571b4357714ee83391
김충현 <winwiewie@gmail.com>
2025-11-13 17:28:26 +0900

    쓸모 없는 임포트 제거

--- src/main/java/staepGame/level2/controller/TwoGameController.java
+++ src/main/java/staepGame/level2/controller/TwoGameController.java
@@ -1,26 +1,12 @@
 package staepGame.level2.controller;
 
 
-import camp.nextstep.edu.missionutils.Randoms;
-import staepGame.level1.model.OneGameAction;
-import staepGame.level1.view.OneGameInputView;
-import staepGame.level1.view.OneGameOutputView;
 import staepGame.level2.view.TwoGameInputView;
 import staepGame.level2.view.TwoGameOutputView;
-import staepGame.total.model.StepRank;
-import staepGame.total.model.User;
+
 import staepGame.total.repository.UserRepository;
-import staepGame.total.validate.UserNameValidate;
-import staepGame.total.validate.UserRcpActionValidate;
-import staepGame.total.validate.UserScissorsFromPaperInputValidate;
-import staepGame.total.validate.YesNoInputValidate;
 import staepGame.total.view.TotalGameInputView;
 import staepGame.total.view.TotalGameOutputView;
-
-import java.util.List;
-
-import static staepGame.total.model.GameCompete.WIN;
-
 
 public class TwoGameController {
 
